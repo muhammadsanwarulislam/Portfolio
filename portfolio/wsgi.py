785f136f00bdf972d4defbd3a77cbb2b5e0773ac
muhammadsanwarulislam <muhammad.sanwarul94@gmail.com>
2018-11-13 04:35:59 +0600

    Initial file

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -16,5 +16,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 application = get_wsgi_application()
 
 # Use whitenoise package to serve static files on Heroku
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application)
